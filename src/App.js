75bff9692cfbbf7e7d7deabcfc4fa7b58fed7c1e
KhaledAlabssi <khaled.absi5@gmail.com>
2021-06-19 12:30:46 +0200

    finishing the app functunality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React, {useState} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import TodoList from "./component/TodoList";
 
+const LS_key = 'reactTodoApp.todos'
 function App() {
-  const [todos, setTodos] = useState([
-    {id: 1, name: 'todo 1', complete: false}
-  ])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function handleAddTodo (e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prev => [{id: Math.random(), name: name, complete: false}, ...prev])
+
+    todoNameRef.current.value = null
+
+  }
+
+  function toggleTodo (id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LS_key))
+    if (storedTodos) setTodos(storedTodos)
+
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LS_key, JSON.stringify(todos))
+
+  }, [todos])
+
+  function handleDeleteCompleted () {
+    const newTodos = todos.filter(t => !t.complete)
+    setTodos(newTodos)
+
+  }
 
 
   return (
     <div className="App">
-      <input type='text' />
-      <button>Add to do</button>
-      <TodoList todos={todos} />
-      <button>Delete completed</button>
-      <p>0 left to do</p>
+      <input type='text' ref={todoNameRef}/>
+      <button onClick={handleAddTodo}>Add to do</button>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <button onClick={handleDeleteCompleted}>Delete completed</button>
+      <p>{todos.filter(t => !t.complete).length} left to do</p>
 
       
     </div>
--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Todo from './Todo'
 
-export default function TodoList({todos}) {
+export default function TodoList({todos, toggleTodo}) {
 
     return (
         <div>
             {todos.map(todo => <Todo 
                                 todo={todo} 
                                 key={todo.id}
+                                toggleTodo={toggleTodo}
                             />)}
             
             
