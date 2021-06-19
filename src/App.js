0e30838d2dc2845b6ad6d3c07a7a5fe83ead3c8d
KhaledAlabssi <khaled.absi5@gmail.com>
2021-06-19 08:24:39 +0200

    testing todo object

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, {useState} from "react";
 import TodoList from "./component/TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, name: 'todo 1', complete: false}
+  ])
+
+
   return (
     <div className="App">
       <input type='text' />
       <button>Add to do</button>
-      <TodoList />
+      <TodoList todos={todos} />
       <button>Delete completed</button>
       <p>0 left to do</p>
 
--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Todo from './Todo'
 
-export default function TodoList() {
+export default function TodoList({todos}) {
 
     return (
         <div>
-            Hi
+            {todos.map(todo => <Todo 
+                                todo={todo} 
+                                key={todo.id}
+                            />)}
+            
             
         </div>
     )
