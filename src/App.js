ff849de2d4ec8d67c4f16cd84c975a6b730236a7
KhaledAlabssi <khaled.absi5@gmail.com>
2021-06-19 08:11:26 +0200

    cleaning unwanted files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import TodoList from "./component/TodoList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' />
+      <button>Add to do</button>
+      <TodoList />
+      <button>Delete completed</button>
+      <p>0 left to do</p>
+
+      
     </div>
   );
 }
