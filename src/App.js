0cd5ee271d4119d782ad4d059c7b1c54950632f5
KhaledAlabssi <khaled.absi5@gmail.com>
2021-06-20 09:50:44 +0200

    cleaning/deleting unwanted code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import TodoList from "./component/TodoList";
 import './App.css'
 
 const LS_key = 'reactTodoApp.todos'
+
 function App() {
+
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
 
@@ -11,9 +13,7 @@ function App() {
     const name = todoNameRef.current.value
     if (name === '') return
     setTodos(prev => [{id: Math.random(), name: name, complete: false}, ...prev])
-
     todoNameRef.current.value = null
-
   }
 
   function toggleTodo (id) {
@@ -31,43 +31,31 @@ function App() {
 
   useEffect(() => {
     localStorage.setItem(LS_key, JSON.stringify(todos))
-
   }, [todos])
 
   function handleDeleteCompleted () {
     const newTodos = todos.filter(t => !t.complete)
     setTodos(newTodos)
-
   }
-
 
   return (
     <div className="App">
       <div className='container'>
-      <div className='top'>
-        <input type='text' ref={todoNameRef} className='check' />
-        <button className='add-button' onClick={handleAddTodo}><h3>Add to do</h3></button>
-
-
-      </div>
-      
-
-      <div className='list'>
-        <TodoList todos={todos} toggleTodo={toggleTodo} />
+        <div className='top'>
+          <input type='text' ref={todoNameRef} className='check' />
+          <button className='add-button' onClick={handleAddTodo}><h3>Add to do</h3></button>
+        </div>
         
+        <div className='list'>
+          <TodoList todos={todos} toggleTodo={toggleTodo} />
+        </div>
 
+        <div className='bottom'>
+        <button className='delete-button' onClick={handleDeleteCompleted}><h3>Delete completed</h3></button>
+          <h5>{todos.filter(t => !t.complete).length} left to do</h5>
+        </div>
 
       </div>
-
-      <div className='bottom'>
-      <button className='delete-button' onClick={handleDeleteCompleted}><h3>Delete completed</h3></button>
-        <h5>{todos.filter(t => !t.complete).length} left to do</h5>
-
-
-      </div>
-      </div>
-      
-
       
     </div>
   );
